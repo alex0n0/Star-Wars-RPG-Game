73f73566b4ed6ca789633b955813de85d4b9275c
alex0n0 <42526086+alex0n0@users.noreply.github.com>
2019-08-22 09:37:22 +1000

    added method to generate selection div's

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,29 @@
-let arrayStarfighters = [
-    new Starfighter(100, 20, 20),
-    new Starfighter(100, 20, 20),
-    new Starfighter(100, 20, 20),
-    new Starfighter(100, 20, 20)
+let arrayData = [
+    ["X-Wing", "./assets/images/starfighter_x_wing.png", "50", "6", "3"],
+    ["Jedi Starfighter", "./assets/images/starfighter_jedi.png", "60", "6", "3"],
+    ["V-19 Torrent Starfighter", "./assets/images/starfighter_v_19torrent.png", "70", "6", "3"],
+    ["TIE Fighter", "./assets/images/starfighter_tie.png", "80", "6", "3"]
 ];
+
+let arrayStarfighters = [];
 
 let phases = ['selectPlayer', 'selectEnemy', 'fight'];
 let phase = phases[0];
+
+let wins = 0;
+let losses = 0;
+
 
 $(document).ready(function () {
     // setup linking UI
     let p_wins = $('#p_wins');
     let p_losses = $('#p_losses');
 
+    p_wins.text(`Wins: ${wins}`);
+    p_losses.text(`Losses: ${losses}`);
 
     let region_selections = $('#region_selections');
-    let starfighter1 = $('#starfighter_1').parent().parent().parent();
-    let starfighter2 = $('#starfighter_2').parent().parent().parent();
-    let starfighter3 = $('#starfighter_3').parent().parent().parent();
-    let starfighter4 = $('#starfighter_4').parent().parent().parent();
+
 
     let region_player = $('#region_player');
     let region_enemies = $('#region_enemies');
@@ -27,88 +32,129 @@ $(document).ready(function () {
     let button_fight = $('#button_fight');
     let region_defender = $('#region_defender');
 
+    let count = 0;
+    for (x of arrayData) {
+        arrayStarfighters[arrayStarfighters.length] = generateStarfighter(x[0], x[1], x[2], x[3], x[4], count)
+        count++;
+    }
+    for (x of arrayStarfighters) {
+        region_selections.append(x[0]);
+    }
 
 
+    console.log(arrayStarfighters);
 
-    let starfighter5 = generateStarfighter('asdf', 100, 3, 14, 5);
-    // region_defender.append(starfighter5);
+
+    let starfighter0 = $('#starfighter_0').parent().parent().parent();
+    let starfighter1 = $('#starfighter_1').parent().parent().parent();
+    let starfighter2 = $('#starfighter_2').parent().parent().parent();
+    let starfighter3 = $('#starfighter_3').parent().parent().parent();
+    
+    function generateStarfighter(name, url, hp, atk, c_Atk, count) {
+        let starfighter_element = $('<div class="col-12 col-sm-6 col-md-3 my-3">');
+        let div_card = $('<div class="card h-100">');
+        starfighter_element.append(div_card);
+    
+        let div_card_top = $('<div class="h-75 card-top">');
+        div_card.append(div_card_top);
+        div_card_top.append($(`<img src="${url}" class="card-img-top" alt="...">`));
+    
+    
+        let div_card_body = $('<div class="card-body bg-dark text-light">');
+        div_card.append(div_card_body);
+        div_card_body.append($(`<h6 class="card-title">${name}</h6>`));
+        div_card_body.append($(`<p class="card-text my-0 starfighter-hp">HP: ${hp}/${hp}</p>`));
+        div_card_body.append($(`<p class="card-text my-0 starfighter-atk">ATK: ${atk}</p>`));
+        div_card_body.append($(`<p class="card-text my-0 starfighter-c-atk">C-ATK: ${c_Atk}</p>`));
+    
+        let a_link = $(`<a id="starfighter_${count}" class="stretched-link"></a>`);
+        div_card_body.append(a_link);
+        
+    
+        return [starfighter_element, a_link, new Starfighter(hp, atk, c_Atk)];
+    }
+
 
 
 
     let player;
     let enemy;
 
+    // resetGame();
 
-    $('#starfighter_1').on('click', clickStarfighter);
-    $('#starfighter_2').on('click', clickStarfighter);
-    $('#starfighter_3').on('click', clickStarfighter);
-    $('#starfighter_4').on('click', clickStarfighter);
+    // function resetGame() {
+    // }
+
+    for (x of arrayStarfighters) {
+        x[1].on('click', clickStarfighter);
+    }
+
 
     function clickStarfighter(e) {
         if (phase === phases[0]) {
             switch (e.target.id) {
+                case 'starfighter_0':
+                    moveStarfighter(arrayStarfighters[0][0], region_player);
+                    arrayStarfighters[0][1].unbind('click', clickStarfighter);
+                    moveStarfighter(arrayStarfighters[1][0], region_enemies);
+                    moveStarfighter(arrayStarfighters[2][0], region_enemies);
+                    moveStarfighter(arrayStarfighters[3][0], region_enemies);
+                    player = arrayStarfighters[0];
+                    break;
                 case 'starfighter_1':
+                    moveStarfighter(starfighter0, region_enemies);
                     moveStarfighter(starfighter1, region_player);
-                    $('#starfighter_1').unbind('click', clickStarfighter);
+                    arrayStarfighters[1][1].unbind('click', clickStarfighter);
                     moveStarfighter(starfighter2, region_enemies);
                     moveStarfighter(starfighter3, region_enemies);
-                    moveStarfighter(starfighter4, region_enemies);
-                    player = starfighter1;
+                    player = arrayStarfighters[1];
                     break;
                 case 'starfighter_2':
+                    moveStarfighter(starfighter0, region_enemies);
                     moveStarfighter(starfighter1, region_enemies);
                     moveStarfighter(starfighter2, region_player);
-                    $('#starfighter_2').unbind('click', clickStarfighter);
+                    arrayStarfighters[2][1].unbind('click', clickStarfighter);
                     moveStarfighter(starfighter3, region_enemies);
-                    moveStarfighter(starfighter4, region_enemies);
-                    player = starfighter2;
+                    player = arrayStarfighters[2];
                     break;
                 case 'starfighter_3':
+                    moveStarfighter(starfighter0, region_enemies);
                     moveStarfighter(starfighter1, region_enemies);
                     moveStarfighter(starfighter2, region_enemies);
                     moveStarfighter(starfighter3, region_player);
-                    $('#starfighter_3').unbind('click', clickStarfighter);
-                    moveStarfighter(starfighter4, region_enemies);
-                    player = starfighter3;
-                    break;
-                case 'starfighter_4':
-                    moveStarfighter(starfighter1, region_enemies);
-                    moveStarfighter(starfighter2, region_enemies);
-                    moveStarfighter(starfighter3, region_enemies);
-                    moveStarfighter(starfighter4, region_player);
-                    $('#starfighter_4').unbind('click', clickStarfighter);
-                    player = starfighter4;
+                    arrayStarfighters[3][1].unbind('click', clickStarfighter);
+                    player = arrayStarfighters[3];
                     break;
             }
-            player.find('.card').toggleClass('card-no-pointer');
+            player[0].find('.card').toggleClass('card-no-pointer');
             region_enemies.toggleClass('bg-danger');
             phase = phases[1];
             console.log(phase);
         } else if (phase === phases[1]) {
             switch (e.target.id) {
+                case 'starfighter_0':
+                    moveStarfighter(starfighter0, region_defender);
+                    $('#starfighter_0').unbind('click', clickStarfighter);
+                    enemy = arrayStarfighters[0];
+                    break;
                 case 'starfighter_1':
                     moveStarfighter(starfighter1, region_defender);
                     $('#starfighter_1').unbind('click', clickStarfighter);
-                    enemy = starfighter1;
+                    enemy = arrayStarfighters[1];
                     break;
                 case 'starfighter_2':
                     moveStarfighter(starfighter2, region_defender);
                     $('#starfighter_2').unbind('click', clickStarfighter);
-                    enemy = starfighter2;
+                    enemy = arrayStarfighters[2];
                     break;
                 case 'starfighter_3':
                     moveStarfighter(starfighter3, region_defender);
                     $('#starfighter_3').unbind('click', clickStarfighter);
-                    enemy = starfighter3;
-                    break;
-                case 'starfighter_4':
-                    moveStarfighter(starfighter4, region_defender);
-                    $('#starfighter_4').unbind('click', clickStarfighter);
-                    enemy = starfighter4;
+                    enemy = arrayStarfighters[3];
                     break;
             }
-            enemy.find('.card').toggleClass('card-no-pointer');
-            moveStarfighter(player, region_attacker);
+            enemy[0].find('.card').toggleClass('card-no-pointer');
+            moveStarfighter(player[0], region_attacker);
             region_enemies.toggleClass('bg-danger');
             phase = phases[2];
             
@@ -121,34 +167,16 @@ $(document).ready(function () {
     }
     function clickFight() {
         console.log('fightfightfight');
-        moveStarfighter(player, region_player);
-        moveStarfighter(enemy, region_enemies);
-        enemy.find('.card').toggleClass('card-disabled');
+        moveStarfighter(player[0], region_player);
+        moveStarfighter(enemy[0], region_enemies);
+        enemy[0].find('.card').toggleClass('card-disabled');
         button_fight.unbind('click', clickFight);
         phase = phases[1];
+        region_enemies.toggleClass('bg-danger');
     }
 });
 
-function generateStarfighter(name, hp, atk, c_Atk, count) {
-    let div_col = $('<div class="col-12 col-sm-6 col-md-3 my-3">');
-    let div_card = $('<div class="card h-100">');
-    div_col.append(div_card);
 
-    let div_card_top = $('<div class="h-75">');
-    div_card.append(div_card_top);
-    div_card_top.append($(`<img src="./assets/images/starfighter_x_wing.png" class="card-img-top" alt="...">`));
-
-
-    let div_card_body = $('<div class="card-body bg-dark text-light">');
-    div_card.append(div_card_body);
-    div_card_body.append($(`<h6 class="card-title">${name}</h6>`));
-    div_card_body.append($(`<p class="card-text my-0">HP: ${hp}/${hp}</p>`));
-    div_card_body.append($(`<p class="card-text my-0">ATK: ${atk}</p>`));
-    div_card_body.append($(`<p class="card-text my-0">C-ATK: ${c_Atk}</p>`));
-    div_card_body.append($(`<a id="starfighter_${count}" class="stretched-link"></a>`));
-
-    return div_col;
-}
 
 
 function moveStarfighter(starship, region) {
